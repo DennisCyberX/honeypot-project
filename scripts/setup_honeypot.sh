424121a64211ed8a8084869f34532ade4f7e10ae
DennisCyberX <dennis.cyberx@gmail.com>
2025-02-02 15:34:35 +0300

    Update setup_honeypot.sh

--- scripts/setup_honeypot.sh
+++ scripts/setup_honeypot.sh
@@ -1,1 +1,7 @@
-
+#!/bin/bash
+echo "Setting up the honeypot..."
+sudo apt update
+sudo apt install python3 python3-pip -y
+pip3 install --upgrade pip
+echo "Dependencies installed."
+echo "To start the honeypot, run: python3 scripts/honeypot.py"
